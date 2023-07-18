efb219d28985e82e0d2a58d3eb0be043c5e77fb9
VoiciehBy <43652981+VoiciehBy@users.noreply.github.com>
2023-07-18 14:51:29 +0200

    Refactoring...

---
+++ js/mkDebug.js
@@ -0,0 +1,7 @@
+function mkDebug() {
+    //DEBUG
+    console.log("aliens[numberOfAliensInARow-1].position.y="+aliens[numberOfAliensInARow-1].position.y);
+    console.log("aliens[0].position.y=" + aliens[0].position.y);
+    console.log("player.position.y="+player.position.y);console.log("i=" + i);
+    console.log("Distance Between alien[0] and Bullet)"+ distanceBetweenAlienAndTheBullet(aliens[0]));
+}mkDebug();
